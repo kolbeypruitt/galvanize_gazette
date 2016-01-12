e429b6ffa21971dc26382247138db15b6f3266ec
kolbeypruitt <kolbeypruitt@gmail.com>
2016-01-12 14:11:09 -0700

    database tied in for posting stories

--- public/javascripts/StoryController.js
+++ public/javascripts/StoryController.js
@@ -2,21 +2,29 @@ angular.module('stories', ['ngRoute'])
   .controller('HomeController', function(){
     
   })
-  .controller('GuitarController', function($http,$scope){
+  .controller('StoryController', function($http,$scope){
+    $scope.hideForm = false;
+
     $http.get('/api/v1/stories').then(function (response) {
-      $scope.stories = response.data
-    })
+      $scope.stories = response.data;
+      })
+
+    $scope.postStory = function () {
+      var story = this.story;
+      $scope.story = {};
+      return $http.post('/api/v1/stories', story ); 
+    }
     
   })
-  .config(function ($routeProvider,$locationProvider) {
-    $routeProvider.when('/', {
-      templateUrl: '/partials/home.html',
-      controller: 'HomeController'
-    })
-    .when('/stories', {
-      templateUrl: '/partials/stories.html',
-      controller: 'GuitarController'
-    })
-    .otherwise('/stories')
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/stories', {
+        templateUrl: '/partials/stories.html',
+        controller: 'StoryController'
+      })
+      .otherwise({
+        redirectTo: '/stories'
+      }
+    )
     $locationProvider.html5Mode(true)
   })
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/gazette');
+var storydb = db.get('storydb');
 
 /* GET home page. */
-router.get('/api/v1/guitars', function (req, res, next) {
-  res.json([
-    {id:1, make: 'Martin', model: 'D15'},
-    {id:2, make: 'Taylor', model: 'DN14'}
-    ])
+router.get('/api/v1/stories', function (req, res, next) {
+  return storydb.find().then(function (stories) {
+    res.json(stories);
+  })
+})
+
+router.post('/api/v1/stories', function (req, res, next) {
+  var story = req.body;
+  return storydb.insert({
+    title: story.title
+    , link: story.link
+    , image: story.image
+    , summary: story.summary
+  }).then(function (story) {
+    console.log(story);
+  })
 })
 router.get('*', function(req, res, next) {
   res.sendFile('index.html', {
